cfa503a8a84d814b71ba7c64ebbe4cf3b27612b0
Matheus Henrique <matheus.181137@gmail.com>
2019-04-21 00:49:48 -0400

    Index route created

---
+++ index.js
@@ -0,0 +1,42 @@
+const bodyParser = require('body-parser'),
+mongoose         = require('mongoose'),
+express          = require('express'),
+app              = express();
+
+// APP CONFIG
+mongoose.connect("mongodb://localhost/restful_blog_app");
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// MONGOOSE/MODEL CONFIG
+const blogSchema = new mongoose.Schema({
+    title: String,
+    image: String,
+    body: String,
+    created: { type: Date, default: Date.now }
+});
+
+const Blog = mongoose.model("Blog", blogSchema);
+
+
+// RESTFUL ROUTES
+
+app.get("/", (req, res) => {
+    res.redirect("/blogs");
+});
+
+app.get("/blogs", (req, res) => {
+    Blog.find({}, (err, blogs) => {
+        if(err) {
+            console.log("ERROR!");
+        } else {
+            res.render("index", { blogs: blogs });
+        }
+    })
+});
+
+app.listen(3000, () => {
+    console.log("The REST Server Has Started");
+});
+  
